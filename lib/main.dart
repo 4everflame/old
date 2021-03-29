91c941b84c1d31a2252081487825510c59f4a89b
4everflame <79952804+4everflame@users.noreply.github.com>
2021-03-29 12:33:23 +0800

    Value notifiers

--- lib/main.dart
+++ lib/main.dart
@@ -1,161 +1,69 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({
+    Key key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    'S21 ULTRA',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Released 2021, January 29',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          FavoriteWidget(),
-        ],
-      ),
-    );
-
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _buildButtonColumn(color, Icons.call, 'CALL'),
-          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-          _buildButtonColumn(color, Icons.share, 'SHARE'),
-        ],
-      ),
-    );
-
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        'Well, the Galaxy S21 Ultra makes up for everything. It has a large 1440p screen with native 120Hz support, adaptive at that. It also brings two dedicated telephoto snappers - one for 3x and another one for 10x optical zoom. Its ultrawide camera does feature autofocus, which works for macro shots. The new Ultra also offers ultrawide-band support, and surprise, surprise, S-Pen support as well. This has to be the first non-Note Galaxy phone to offer that - one for the history books.',
-        softWrap: true,
-      ),
-    );
-
     return MaterialApp(
-      title: 'Phone specification',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Phone specification'),
-        ),
-        body: ListView(
-          children: [
-            Image.asset(
-              'images/s21ultra.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection,
-          ],
-        ),
+        body: BodyWidget(),
       ),
     );
   }
-
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
 }
 
-// #docregion FavoriteWidget
-class FavoriteWidget extends StatefulWidget {
-  @override
-  _FavoriteWidgetState createState() => _FavoriteWidgetState();
-}
-// #enddocregion FavoriteWidget
+class BodyWidget extends StatelessWidget {
 
-// #docregion _FavoriteWidgetState, _FavoriteWidgetState-fields, _FavoriteWidgetState-build
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  // #enddocregion _FavoriteWidgetState-build
-  bool _isFavorited = true;
-  int _favoriteCount = 41;
-  // #enddocregion _FavoriteWidgetState-fields
+  //String fruit = 'Apples';
+  final colorNotfier = ValueNotifier<Color>(Colors.purple.shade700);
 
-  // #docregion _toggleFavorite
-  void _toggleFavorite() {
-    setState(() {
-      if (_isFavorited) {
-        _favoriteCount -= 1;
-        _isFavorited = false;
-      } else {
-        _favoriteCount += 1;
-        _isFavorited = true;
-      }
-    });
-  }
-  // #enddocregion _toggleFavorite
-
-  // #docregion _FavoriteWidgetState-build
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          padding: EdgeInsets.all(0),
-          child: IconButton(
-            padding: EdgeInsets.all(0),
-            alignment: Alignment.centerRight,
-            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
-            color: Colors.red[500],
-            onPressed: _toggleFavorite,
+    return ValueListenableBuilder<Color>(
+      valueListenable: colorNotfier,
+    builder: (context, color, child) {
+        return Container(
+          width: double.infinity,
+              height: double.infinity,
+            color: color,
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(width: 100),
+                Text('Hello'),
+                SizedBox(height: 100),
+                ElevatedButton(
+                  onPressed: () {
+                    print('I was clicked');
+                    final randomNumber = Random().nextInt(6);
+                    final myNewColor = colorList[randomNumber];
+
+                    colorNotfier.value = myNewColor;
+                  },
+                  child: Text('Click me'),
+            ),
+  ]
           ),
         ),
-        SizedBox(
-          width: 18,
-          child: Container(
-            child: Text('$_favoriteCount'),
-          ),
-        ),
-      ],
     );
-  }
-// #docregion _FavoriteWidgetState-fields
+  },
+  );
 }
+}
+final colorList = <Color>[
+  Colors.purple,
+  Colors.white,
+  Colors.orange,
+  Colors.blue,
+  Colors.red,
+  Colors.pink,
+  Colors.lightBlueAccent,
+];
