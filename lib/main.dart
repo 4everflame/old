2de80022f4bac3417db2b32de3967429ac8cc3c2
4everflame <79952804+4everflame@users.noreply.github.com>
2021-03-31 00:21:49 +0800

    State management

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
 import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_playground/home_page/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,50 +22,5 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class BodyWidget extends StatelessWidget {
 
-  //String fruit = 'Apples';
-  final colorNotfier = ValueNotifier<Color>(Colors.purple.shade700);
 
-  @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder<Color>(
-      valueListenable: colorNotfier,
-    builder: (context, color, child) {
-        return Container(
-          width: double.infinity,
-              height: double.infinity,
-            color: color,
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(width: 100),
-                Text('Hello'),
-                SizedBox(height: 100),
-                ElevatedButton(
-                  onPressed: () {
-                    print('I was clicked');
-                    final randomNumber = Random().nextInt(6);
-                    final myNewColor = colorList[randomNumber];
-
-                    colorNotfier.value = myNewColor;
-                  },
-                  child: Text('Click me'),
-            ),
-  ]
-          ),
-        ),
-    );
-  },
-  );
-}
-}
-final colorList = <Color>[
-  Colors.purple,
-  Colors.white,
-  Colors.orange,
-  Colors.blue,
-  Colors.red,
-  Colors.pink,
-  Colors.lightBlueAccent,
-];
